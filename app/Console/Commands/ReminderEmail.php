2449963e913e8c8a224073f2b5a71a63740d51bf
sagar.jayani <sagar.jayani@bizotics.com>
2020-10-19 13:23:10 +0530

    reminder commond added

---
+++ app/Console/Commands/ReminderEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ReminderEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reminder:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reminder date and time match send email';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $count = \DB::table('users')
+            ->whereDate('created_at', '=', date('Y-m-d'))
+            ->count();
+ 
+            echo "Today $count users registered";
+    }
+}
